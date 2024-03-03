2114fe0945dd008bebcda6e614fb77a8d6fe8103
Joseph Jenkins <amjosephjenkins@gmail.com>
2024-03-03 16:46:20 +0100

    facebook and calendar added

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -2,7 +2,10 @@
   <section class="___section contact display" ref="contact" v-if="contactsOpened">
     <div class="contact___text">
       <h2 class="header___text contact___main___text">Book a call with us.</h2>
-      <a class="booking___btn" href="https://calendar.app.google/CPECws29tbeW6Hye8"
+      <a
+        @click="handleClick"
+        class="booking___btn"
+        href="https://calendar.app.google/CPECws29tbeW6Hye8"
         >Book an appointment</a
       >
       <h2 class="header___text contact___main___text">Or contact us.</h2>
@@ -33,6 +36,11 @@ export default {
   },
   components: {
     SocialLink: defineAsyncComponent(() => import('./SocialLink.vue'))
+  },
+  methods: {
+    handleClick() {
+      fbq('track', 'Booked', { value: 'yes' })
+    }
   }
 }
 </script>

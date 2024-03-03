3671f1732380b6b1b66e36b01cac770e39b76d21
Joseph Jenkins <amjosephjenkins@gmail.com>
2024-03-03 16:42:40 +0100

    facebook and calendar added

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,10 +1,15 @@
 <template>
   <section class="___section contact display" ref="contact" v-if="contactsOpened">
     <div class="contact___text">
-      <h2 class="header___text contact___main___text">Contact us.</h2>
+      <h2 class="header___text contact___main___text">Book a call with us.</h2>
+      <a class="booking___btn" href="https://calendar.app.google/CPECws29tbeW6Hye8"
+        >Book an appointment</a
+      >
+      <h2 class="header___text contact___main___text">Or contact us.</h2>
       <div class="contact___links">
         <div class="display___flex">
           <SocialLink link="mailto:offedgestudio@gmail.com" placeholder="Email" />
+          <SocialLink link="https://www.facebook.com/offedgestudio" placeholder="Facebook" />
           <SocialLink link="https://www.instagram.com/off___edge" placeholder="Instagram" />
           <SocialLink link="https://www.linkedin.com/company/off-edge" placeholder="Linkedin" />
           <SocialLink link="https://www.twitter.com/off___edge" placeholder="Twitter" />
